2aef3fb87975e05c774da362705cce1b9f382f1a
CadmusKei <maxwell.kei.farouk@gmail.com>
2026-03-11 14:14:58 +0200

    Start reading Method

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -7,13 +7,30 @@ import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public class Anagrams {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+
+
         HashMap<String, ArrayList<String>> anagrams = new HashMap<>();
 
     }
 
-    
+    public static ArrayList<String> readBook(String filename)  throws IOException {
+        Path path = Path.of(filename);
+        ArrayList<String> wordArray = new ArrayList<>();
+
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(line -> {
+                String[] words = line.split("\\s");
+                wordArray.add(word);
+            });
+        }
+
+    }
+
+
+
+
+
 
 
 
