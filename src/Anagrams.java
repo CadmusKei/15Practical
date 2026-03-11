d84eab4a5a85287e037db9fddfff2e24815a0e5f
CadmusKei <maxwell.kei.farouk@gmail.com>
2026-03-11 14:20:42 +0200

    Add book file and finsish reading method, read array for debug

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,4 +1,5 @@
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.nio.file.Files;
@@ -9,22 +10,25 @@ import java.util.stream.Stream;
 public class Anagrams {
     public static void main(String[] args) throws IOException {
 
-
         HashMap<String, ArrayList<String>> anagrams = new HashMap<>();
+        ArrayList<String> words = readBook("joyce1922_ulysses-1.text");
+        System.out.println(words);
 
     }
 
     public static ArrayList<String> readBook(String filename)  throws IOException {
+        if (filename.isEmpty()) return new ArrayList<>();
         Path path = Path.of(filename);
         ArrayList<String> wordArray = new ArrayList<>();
-
         try (Stream<String> lines = Files.lines(path)) {
             lines.forEach(line -> {
                 String[] words = line.split("\\s");
-                wordArray.add(word);
+                for (String word : words) {
+                    wordArray.add(word);
+                }
             });
         }
-
+        return (wordArray);
     }
 
 
