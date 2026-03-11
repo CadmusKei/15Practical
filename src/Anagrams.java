7acb599f5f18e70c79985df14455c44d37a3d073
CadmusKei <maxwell.kei.farouk@gmail.com>
2026-03-11 14:08:43 +0200

    Add dependancies (imports)

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -1,5 +1,20 @@
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class Anagrams {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        HashMap<String, ArrayList<String>> anagrams = new HashMap<>();
 
     }
+
+    
+
+
+
 }
